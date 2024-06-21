3a8ed0e9028a501a1f8c2f6a544815fe86787706
Lalitchdhary01 <Lalitkumar46789@gmail.com>
2024-06-21 14:08:02 +0530

    wrapm up backend

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import databaseConnection from "./config/database.js";
 import cookieParser from "cookie-parser";
 import userRoute from "./routes/userRoute.js";
 import tweetRoute from "./routes/tweetRoute.js";
+import cors from "cors";
 
 dotenv.config({ path: ".env" });
 
@@ -15,6 +16,11 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
+const corsOptions = {
+  origin: "http://localhost:3001",
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
 //api
 app.use("/api/v1/user", userRoute);
